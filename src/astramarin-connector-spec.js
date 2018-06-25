2a59ecb9a6826bbac0c2f10ddcea99efc89883fc
Realetive <realetive@yandex.ru>
2018-06-26 00:41:21 +0300

    major(api): Update API to class

---
+++ src/astramarin-connector-spec.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/* eslint-env mocha */
+const assert = require('assert')
+const astramarinConnector = require('.')
+
+describe('astramarin-connector', () => {
+  it('write this test', () => {
+    // console.assert(astramarinConnector, 'should export something')
+    assert.equal(typeof astramarinConnector, 'object')
+  })
+})
